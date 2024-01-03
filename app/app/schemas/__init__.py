7723e9c91d1fb37a1bdac1538043bfafd15e4a6b
e-kondr01 <e.kondr01@gmail.com>
2024-01-03 18:43:00 +0300

    feat: add CQRS list endpoint

--- app/app/schemas/__init__.py
+++ app/app/schemas/__init__.py
@@ -1,1 +1,18 @@
+from uuid import UUID
 
+from pydantic import BaseModel
+
+
+class UnitSchema(BaseModel):
+    id: UUID
+    name: str
+
+
+class StudentSchema(BaseModel):
+    id: UUID
+    name: str
+
+
+class StudentProgressSchema(BaseModel):
+    current_unit: UnitSchema
+    student: StudentSchema
--- app/app/schemas/student_progress.py
+++
@@ -1,8 +0,0 @@
-from uuid import UUID
-
-from pydantic import BaseModel
-
-
-class StudentProgressSchema(BaseModel):
-    current_unit_id: UUID
-    student_id: UUID
